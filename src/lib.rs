0908d1cfd1e06eb475ec5a556b3e36d916a2e55e
Fraser Balch <farseer.ulwithy@gmail.com>
2021-07-21 00:06:13 -0600

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,478 @@
+use gen_id_allocator::{UntypedId, ValidId};
+use iter_context::{ContextualIterator, Iter, IterMut};
+use my_derives::MyDefault;
+use ref_cast::RefCast;
+use std::marker::PhantomData;
+use std::ops::{Index, IndexMut, Neg, Not};
+
+pub use from_iter::FromContextualIterator;
+
+mod from_iter;
+
+#[repr(transparent)]
+#[derive(Debug, RefCast)]
+pub struct UntypedComponent<T> {
+    values: Vec<T>,
+}
+
+impl<T> Default for UntypedComponent<T> {
+    #[inline]
+    fn default() -> Self {
+        Self {
+            values: Default::default(),
+        }
+    }
+}
+
+impl<T: Clone> Clone for UntypedComponent<T> {
+    #[inline]
+    fn clone(&self) -> Self {
+        Self {
+            values: self.values.clone(),
+        }
+    }
+
+    #[inline]
+    fn clone_from(&mut self, source: &Self) {
+        self.values.clone_from(&source.values);
+    }
+}
+
+impl<T> UntypedComponent<T> {
+    #[inline]
+    pub fn insert(&mut self, id: UntypedId, value: T) {
+        let index = id.index();
+        match self.values.get_mut(index) {
+            None => {
+                assert_eq!(
+                    index,
+                    self.values.len(),
+                    "UntypedComponent::insert: invalid index"
+                );
+                self.values.push(value);
+            }
+            Some(current) => *current = value,
+        }
+    }
+
+    #[inline]
+    pub fn get(&self, id: UntypedId) -> Option<&T> {
+        self.values.get(id.index())
+    }
+
+    #[inline]
+    pub fn get_mut(&mut self, id: UntypedId) -> Option<&mut T> {
+        self.values.get_mut(id.index())
+    }
+
+    #[inline]
+    pub fn swap(&mut self, a: UntypedId, b: UntypedId) {
+        self.values.swap(a.index(), b.index());
+    }
+
+    #[inline]
+    pub fn fill_with<F: FnMut() -> T>(&mut self, f: F) {
+        self.values.fill_with(f);
+    }
+
+    #[inline]
+    pub fn iter(&self) -> std::slice::Iter<T> {
+        self.values.iter()
+    }
+
+    #[inline]
+    pub fn iter_mut(&mut self) -> std::slice::IterMut<T> {
+        self.values.iter_mut()
+    }
+
+    #[inline]
+    pub fn len(&self) -> usize {
+        self.values.len()
+    }
+
+    #[inline]
+    pub fn is_empty(&self) -> bool {
+        self.len() == 0
+    }
+}
+
+impl<T> From<Vec<T>> for UntypedComponent<T> {
+    #[inline]
+    fn from(values: Vec<T>) -> Self {
+        UntypedComponent { values }
+    }
+}
+
+impl<'a, T> IntoIterator for &'a UntypedComponent<T> {
+    type Item = &'a T;
+    type IntoIter = std::slice::Iter<'a, T>;
+
+    #[inline]
+    fn into_iter(self) -> Self::IntoIter {
+        self.values.iter()
+    }
+}
+
+impl<'a, T> IntoIterator for &'a mut UntypedComponent<T> {
+    type Item = &'a mut T;
+    type IntoIter = std::slice::IterMut<'a, T>;
+
+    #[inline]
+    fn into_iter(self) -> Self::IntoIter {
+        self.values.iter_mut()
+    }
+}
+
+impl<T> Index<UntypedId> for UntypedComponent<T> {
+    type Output = T;
+
+    #[inline]
+    fn index(&self, index: UntypedId) -> &Self::Output {
+        self.values.index(index.index())
+    }
+}
+
+impl<T> IndexMut<UntypedId> for UntypedComponent<T> {
+    #[inline]
+    fn index_mut(&mut self, index: UntypedId) -> &mut Self::Output {
+        self.values.index_mut(index.index())
+    }
+}
+
+impl<T> Index<&UntypedId> for UntypedComponent<T> {
+    type Output = T;
+
+    #[inline]
+    fn index(&self, index: &UntypedId) -> &Self::Output {
+        self.values.index(index.index())
+    }
+}
+
+impl<T> IndexMut<&UntypedId> for UntypedComponent<T> {
+    #[inline]
+    fn index_mut(&mut self, index: &UntypedId) -> &mut Self::Output {
+        self.values.index_mut(index.index())
+    }
+}
+
+#[repr(transparent)]
+#[derive(Debug, MyDefault, RefCast)]
+pub struct Component<Arena, T> {
+    values: UntypedComponent<T>,
+    marker: PhantomData<Arena>,
+}
+
+impl<Arena, T: Clone> Clone for Component<Arena, T> {
+    #[inline]
+    fn clone(&self) -> Self {
+        Self {
+            values: self.values.clone(),
+            marker: PhantomData,
+        }
+    }
+
+    #[inline]
+    fn clone_from(&mut self, source: &Self) {
+        self.values.clone_from(&source.values);
+    }
+}
+
+impl<Arena, T> From<Vec<T>> for Component<Arena, T> {
+    #[inline]
+    fn from(values: Vec<T>) -> Self {
+        Component {
+            values: values.into(),
+            marker: PhantomData,
+        }
+    }
+}
+
+impl<Arena, T> Component<Arena, T> {
+    #[inline]
+    pub fn insert(&mut self, id: impl ValidId<Arena = Arena>, value: T) {
+        self.values.insert(id.id().untyped, value);
+    }
+
+    #[inline]
+    pub fn get(&self, id: impl ValidId<Arena = Arena>) -> Option<&T> {
+        self.values.get(id.id().untyped)
+    }
+
+    #[inline]
+    pub fn get_mut(&mut self, id: impl ValidId<Arena = Arena>) -> Option<&mut T> {
+        self.values.get_mut(id.id().untyped)
+    }
+
+    #[inline]
+    pub fn swap<A: ValidId<Arena = Arena>, B: ValidId<Arena = Arena>>(&mut self, a: A, b: B) {
+        self.values.swap(a.id().untyped, b.id().untyped);
+    }
+
+    #[inline]
+    pub fn fill_with<F: FnMut() -> T>(&mut self, f: F) {
+        self.values.fill_with(f);
+    }
+
+    #[inline]
+    pub fn iter(&self) -> Iter<Arena, T> {
+        Iter::new(self.values.iter())
+    }
+
+    #[inline]
+    pub fn iter_mut(&mut self) -> IterMut<Arena, T> {
+        IterMut::new(self.values.iter_mut())
+    }
+
+    #[inline]
+    pub fn len(&self) -> usize {
+        self.values.len()
+    }
+
+    #[inline]
+    pub fn is_empty(&self) -> bool {
+        self.len() == 0
+    }
+}
+
+impl<Arena, T> Component<Arena, Option<T>> {
+    #[inline]
+    pub fn remove(&mut self, id: impl ValidId<Arena = Arena>) -> Option<T> {
+        let value = self.values.get_mut(id.id().untyped)?;
+        std::mem::take(value)
+    }
+}
+
+impl<'a, Arena, T: Copy + 'a> Component<Arena, T> {
+    #[inline]
+    pub fn assign<Rhs: ContextualIterator<Context = Arena, Item = T>>(&mut self, rhs: Rhs) {
+        self.zip(rhs).for_each(|(lhs, rhs)| *lhs = rhs);
+    }
+
+    #[inline]
+    pub fn assign_ref<Rhs: ContextualIterator<Context = Arena, Item = &'a T>>(&mut self, rhs: Rhs) {
+        self.zip(rhs).for_each(|(lhs, rhs)| *lhs = *rhs);
+    }
+}
+
+impl<Arena, T, V: ValidId<Arena = Arena>> Index<V> for Component<Arena, T> {
+    type Output = T;
+
+    #[inline]
+    fn index(&self, index: V) -> &Self::Output {
+        self.values.index(index.id().untyped)
+    }
+}
+
+impl<Arena, T, V: ValidId<Arena = Arena>> IndexMut<V> for Component<Arena, T> {
+    #[inline]
+    fn index_mut(&mut self, index: V) -> &mut Self::Output {
+        self.values.index_mut(index.id().untyped)
+    }
+}
+
+impl<'a, Arena, T> IntoIterator for &'a Component<Arena, T> {
+    type Item = &'a T;
+    type IntoIter = std::slice::Iter<'a, T>;
+
+    #[inline]
+    fn into_iter(self) -> Self::IntoIter {
+        self.values.iter()
+    }
+}
+
+impl<'a, Arena, T> IntoIterator for &'a mut Component<Arena, T> {
+    type Item = &'a mut T;
+    type IntoIter = std::slice::IterMut<'a, T>;
+
+    #[inline]
+    fn into_iter(self) -> Self::IntoIter {
+        self.values.iter_mut()
+    }
+}
+
+impl<'a, Arena, T> ContextualIterator for &'a Component<Arena, T> {
+    type Context = Arena;
+}
+
+impl<'a, Arena, T> ContextualIterator for &'a mut Component<Arena, T> {
+    type Context = Arena;
+}
+
+macro_rules! impl_op_assign {
+    ($op_trait:ident, $op_fn:ident) => {
+        impl<Arena, T, Rhs> std::ops::$op_trait<Rhs> for $crate::Component<Arena, T>
+        where
+            Rhs: iter_context::ContextualIterator<Context = Arena>,
+            T: std::ops::$op_trait<Rhs::Item>,
+        {
+            #[inline]
+            fn $op_fn(&mut self, rhs: Rhs) {
+                self.zip(rhs).for_each(|(lhs, rhs)| lhs.$op_fn(rhs));
+            }
+        }
+    };
+}
+
+impl_op_assign!(AddAssign, add_assign);
+impl_op_assign!(SubAssign, sub_assign);
+impl_op_assign!(MulAssign, mul_assign);
+impl_op_assign!(DivAssign, div_assign);
+impl_op_assign!(RemAssign, rem_assign);
+impl_op_assign!(BitAndAssign, bitand_assign);
+impl_op_assign!(BitOrAssign, bitor_assign);
+impl_op_assign!(BitXorAssign, bitxor_assign);
+impl_op_assign!(ShlAssign, shl_assign);
+impl_op_assign!(ShrAssign, shr_assign);
+
+macro_rules! impl_op {
+    ($op_trait:ident, $op_fn:ident) => {
+        impl<'a, Arena, T, Rhs, Output> std::ops::$op_trait<Rhs> for &'a $crate::Component<Arena, T>
+        where
+            &'a T: std::ops::$op_trait<Rhs::Item, Output = Output>,
+            Rhs: iter_context::ContextualIterator<Context = Arena>,
+        {
+            type Output = iter_context::Map<
+                Arena,
+                iter_context::Zip<Arena, Self, Rhs>,
+                fn((&'a T, Rhs::Item)) -> Output,
+            >;
+
+            #[inline]
+            fn $op_fn(self, rhs: Rhs) -> Self::Output {
+                self.zip(rhs).map(|(lhs, rhs)| lhs.$op_fn(rhs))
+            }
+        }
+    };
+}
+
+impl_op!(Add, add);
+impl_op!(Sub, sub);
+impl_op!(Mul, mul);
+impl_op!(Div, div);
+impl_op!(Rem, rem);
+impl_op!(Shl, shl);
+impl_op!(Shr, shr);
+impl_op!(BitAnd, bitand);
+impl_op!(BitOr, bitor);
+impl_op!(BitXor, bitxor);
+
+impl<'a, Arena, T> Not for &'a Component<Arena, T>
+where
+    &'a T: Not,
+{
+    type Output = iter_context::Map<Arena, Self, fn(&'a T) -> <&'a T as Not>::Output>;
+
+    #[inline]
+    fn not(self) -> Self::Output {
+        self.map(Not::not)
+    }
+}
+
+impl<'a, Arena, T> Neg for &'a Component<Arena, T>
+where
+    &'a T: Neg,
+{
+    type Output = iter_context::Map<Arena, Self, fn(&'a T) -> <&'a T as Neg>::Output>;
+
+    #[inline]
+    fn neg(self) -> Self::Output {
+        self.map(Neg::neg)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use gen_id_allocator::{Id, Valid};
+
+    fn get_id(i: usize) -> Valid<'static, Id<()>> {
+        Valid::assert(Id::first(i))
+    }
+
+    #[test]
+    #[should_panic]
+    fn insert_beyond_end_panics() {
+        let mut comp = Component::<(), ()>::default();
+        let id = get_id(1);
+
+        comp.insert(id, ());
+    }
+
+    #[allow(dead_code)]
+    fn assignment_ops(comp: &Component<(), u32>, target: &mut Component<(), u32>) {
+        *target += comp + comp;
+        *target += comp;
+        *target -= comp;
+        *target *= comp;
+        *target /= comp;
+        *target %= comp;
+        *target &= comp;
+        *target |= comp;
+        *target ^= comp;
+        *target <<= comp;
+        *target >>= comp;
+    }
+
+    #[test]
+    fn add_then_add_assign() {
+        let mut ones = Component::<(), u32>::default();
+        let mut target = Component::<(), u32>::default();
+        let id = get_id(0);
+        ones.insert(id, 1);
+        target.insert(id, 0);
+
+        target += &ones + &ones;
+
+        assert_eq!(2, target[id]);
+    }
+
+    #[test]
+    fn assign() {
+        let mut ones = Component::<(), u32>::default();
+        let mut target = Component::<(), u32>::default();
+        let id = get_id(0);
+        ones.insert(id, 1);
+        target.insert(id, 0);
+
+        target.assign(&ones + &ones);
+
+        assert_eq!(2, target[id]);
+    }
+
+    #[test]
+    fn assign_ref() {
+        let mut ones = Component::<(), u32>::default();
+        let mut target = Component::<(), u32>::default();
+        let id = get_id(0);
+        ones.insert(id, 1);
+        target.insert(id, 0);
+
+        target.assign_ref(&ones);
+
+        assert_eq!(1, target[id]);
+    }
+
+    #[test]
+    fn lots_of_math() {
+        let mut target = Component::<(), u32>::from(vec![0, 0, 0]);
+        let ints = Component::<(), u32>::from(vec![1, 2, 3]);
+        let primes = Component::<(), u32>::from(vec![2, 3, 5]);
+
+        target.assign((&primes + &ints) * &primes);
+
+        assert_eq!(target.values.values, vec![6, 15, 40]);
+    }
+
+    #[test]
+    fn remove() {
+        let id = get_id(0);
+        let mut comp = Component::<(), Option<u32>>::default();
+
+        assert_eq!(None, comp.remove(id));
+
+        comp.insert(id, Some(5));
+
+        assert_eq!(Some(5), comp.remove(id));
+        assert_eq!(None, comp.remove(id));
+    }
+}
